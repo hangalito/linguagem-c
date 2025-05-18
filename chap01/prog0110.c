842f23c4325e2c52390dcaa18d7ed9bf7e997a0f
Bartolomeu Hangalo <bartolomeujose.manilson@gmail.com>
2025-05-18 18:05:54 +0100

    resolução dos exercícios do capítulo 1

---
+++ chap01/prog0110.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main()
+{
+    printf("Total \t = \t 100%%\n");
+    printf("IVA \t = \t 17%%\n");
+    printf("IRS \t = \t 15%%\n");
+    printf("----------------------\n");
+    printf("Líq. \t = \t 68%%");
+}
