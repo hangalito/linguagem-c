70075264ebbaa52e588f4649d6fb1897db748ac2
Bartolomeu Hangalo <bartolomeujose.manilson@gmail.com>
2025-06-26 12:50:52 +0100

    capítulo 5: introdução

---
+++ chap05/README.md
@@ -0,0 +1,12 @@
+Capítulo 5 - Funções e Procedimentos
+============================================================
+
+# Objectivos
+ * Funções e procedimentos em C
+ * Cabeçalho e corpo de um função
+ * Parâmetros
+ * Passagem de parâmetros de tipos básicos
+ * Valor de retorno (*return*)
+ * O "tipo" *void*
+ * Protótipos e funções
+ * Variáveis locais
---
+++ chap05/prog0501.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+main()
+{
+    int i;
+
+    // Escrita do cabeçalho
+    for (i = 1; i <= 20; i++)
+        putchar('*');
+    putchar('\n');
+
+    puts("Numeros entre 1 e 5");
+
+    for (i = 1; i <= 20; i++)
+        putchar('*');
+    putchar('\n');
+
+    // Escrita dos numeros
+    for (i = 1; i <= 5; i++)
+        printf("%d\n", i);
+
+    for (i = 1; i <= 20; i++)
+        putchar('*');
+    putchar('\n');
+}
